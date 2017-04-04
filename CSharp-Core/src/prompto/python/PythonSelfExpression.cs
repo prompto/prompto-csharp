8e578547c3f8001c153c7fba5dbddafde264c5e1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-04-04 15:14:54 +0800

    support PythonSelfExpression

---
+++ CSharp-Core/src/prompto/python/PythonSelfExpression.cs
@@ -0,0 +1,18 @@
+﻿using prompto.runtime;
+using System;
+using prompto.utils;
+using prompto.expression;
+
+
+namespace prompto.java
+{
+
+	public class JavaThisExpression : JavaExpression
+	{
+
+		public void ToDialect(CodeWriter writer) {
+			writer.append("this");
+		}
+
+	}
+}
