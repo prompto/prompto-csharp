33c8f41570dd15efdec50b89b8cc630cbe3e997e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-06-02 16:17:46 +0200

    add sysGetEnvironmentVariable

---
+++ CSharp-Tests/src/prompto/library/e/TestSystem.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using prompto.parser;
+using prompto.utils;
+using prompto.runtime;
+
+namespace prompto.library.e
+{
+
+	[TestFixture]
+	public class TestSystem : BaseEParserTest
+	{
+
+		[SetUp]
+		public void before()
+		{
+			Loader.Load ();
+			Out.init();
+			coreContext = null;
+			LoadDependency("system");
+			LoadDependency("core");
+		}
+
+		[TearDown]
+		public void after()
+		{
+			Out.restore();
+		}
+
+		[Test]
+		public void testSystem()
+		{
+			CheckTests("system/system.pec");
+		}
+
+	}
+}
+
