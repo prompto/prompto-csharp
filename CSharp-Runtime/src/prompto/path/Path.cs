d58d43782bf4bba20bf5f854606d40f72aec4da9
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-06-24 12:03:55 +0200

    more libs

--- CSharp-Runtime/src/prompto/path/Path.cs
+++ CSharp-Runtime/src/prompto/path/Path.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 
 namespace prompto.path
@@ -36,6 +37,42 @@ namespace prompto.path
 		{
 			FileInfo info = new FileInfo(path);
 			return info.Exists && info.Attributes.HasFlag(FileAttributes.ReparsePoint);
+		}
+
+		public static String compressToTempPath(String path)
+        {
+			FileInfo rawFile = new FileInfo(path);
+			using (FileStream rawInput = rawFile.OpenRead())
+            {
+				String compressedPath = System.IO.Path.ChangeExtension(System.IO.Path.GetTempFileName(), ".gz");
+				FileInfo compressedFile = new FileInfo(compressedPath);
+				using (FileStream compressedOutput = compressedFile.OpenWrite())
+                {
+					using (GZipStream deflatingOutput = new GZipStream(compressedOutput, CompressionMode.Compress))
+                    {
+						rawInput.CopyTo(deflatingOutput);
+						return compressedPath;
+					}
+                }
+			}
+		}
+
+		public static String decompressToTempPath(String path)
+		{
+			FileInfo compressedFile = new FileInfo(path);
+			using (FileStream compressedInput = compressedFile.OpenRead())
+			{
+				using (GZipStream inflatingInput = new GZipStream(compressedInput, CompressionMode.Decompress))
+				{
+					String rawPath = System.IO.Path.ChangeExtension(System.IO.Path.GetTempFileName(), ".raw");
+					FileInfo rawFile = new FileInfo(rawPath);
+					using (FileStream rawOutput = rawFile.OpenWrite())
+					{
+						inflatingInput.CopyTo(rawOutput);
+						return rawPath;
+					}
+				}
+			}
 		}
 	}
 }
